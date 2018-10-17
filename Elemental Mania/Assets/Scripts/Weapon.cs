90027eed17d7f474f7a64e0a4fe6670dae74ef47
Rasmus Dalsgaard Olsen <PRW687@motorolasolutions.com>
2018-10-17 14:01:15 +0200

    Weapon.cs was missing cooldown

--- Elemental Mania/Assets/Scripts/Weapon.cs
+++ Elemental Mania/Assets/Scripts/Weapon.cs
@@ -10,10 +10,10 @@ public class Weapon : MonoBehaviour {
     public int Cooldown;
 
     private ParticleSystem m_Emitter;
+    private float update;
 
-
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         m_Emitter = GetComponent<ParticleSystem>();
         Assert.IsTrue(m_Emitter.collision.enabled, "Must have an emitter with collisions enabled");        
 	}
@@ -22,8 +22,10 @@ public class Weapon : MonoBehaviour {
 	void Update () {
         if (Input.GetButtonDown("PrimaryFire"))
         {
-            if(Time.deltaTime > 1.0f)
+            update += Time.deltaTime;
+            if (update > Cooldown)
             {
+                update = 0.0f;
                 m_Emitter.Play();
             }
         }
