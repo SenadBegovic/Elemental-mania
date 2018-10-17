f226209b390b84245bff3a515b8d09e71e6f91db
Sebastian Hjorth <sebastianh@unity3d.com>
2018-10-17 14:29:13 +0200

    Made 2player input

---
+++ Elemental Mania/Assets/Scripts/InputMapping.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu( fileName = "InputMapping", menuName = "Input/Mapping")]
+public class InputMapping : ScriptableObject {
+    public string kHorizontalMovement;
+    public string kVerticalMovement;
+    public string kPrimaryFire;
+    public string kSecondaryFire;
+    public string kNextWeapon;
+}
