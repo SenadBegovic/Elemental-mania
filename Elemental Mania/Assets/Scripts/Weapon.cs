6a3569e4aad2c46f7785672a2270d755c73f72ec
Sebastian Hjorth <sebastianh@unity3d.com>
2018-10-17 13:16:42 +0200

    Added weapon script

---
+++ Elemental Mania/Assets/Scripts/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Weapon : MonoBehaviour {
+
+    public int Damage;
+    public ElementalType type;
+    public int Cooldown;
+
+    private ParticleSystem m_Emitter;
+
+
+	// Use this for initialization
+	void Start () {
+        Assert.IsTrue(m_Emitter.collision.enabled, "Must have an emitter with collisions enabled");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
