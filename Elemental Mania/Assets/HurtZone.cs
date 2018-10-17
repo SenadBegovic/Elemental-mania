927da53a225683d488dc88027bcef3453bac3662
Sebastian Hjorth <sebastianh@unity3d.com>
2018-10-17 13:57:28 +0200

    Added elemental walls behaviour

---
+++ Elemental Mania/Assets/HurtZone.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtZone : MonoBehaviour {
+    public int DamagePerSecond;
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        int damageToDeal = (int)(Time.deltaTime * DamagePerSecond);
+
+        // TODO: Get player and deal damage
+    }
+}
---
+++ Elemental Mania/Assets/Scripts/Resistance.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Resistance {
+    [Range(0, 100)]
+    public int Fire;
+
+    [Range(0, 100)]
+    public int Frost;
+
+    [Range(0, 100)]
+    public int Wind;
+
+    [Range(0, 100)]
+    public int Earth;
+
+    public int GetResistance(ElementalType type)
+    {
+        switch (type)
+        {
+            case ElementalType.EARTH:
+                return Earth;
+            case ElementalType.FIRE:
+                return Fire;
+            case ElementalType.WIND:
+                return Wind;
+            case ElementalType.FROST:
+                return Frost;
+        }
+        throw new System.Exception("Unknown elemental type");
+    }
+}
