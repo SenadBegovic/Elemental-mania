38eae62f782d61fbbf4319caaccfb55506ce23cf
Rasmus <rdobrandelev@gmail.com>
2018-10-23 15:32:27 +0200

    Merge with master to fix collision

---
+++ Elemental Mania/Assets/Scripts/WallStats.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "WallPresets")]
+public class WallStats : ScriptableObject {
+
+    [SerializeField]
+    private ElementalType ktype;
+    [SerializeField]
+    private int kMaxHealth;
+    [SerializeField]
+    private Resistance kResistance;
+
+}
