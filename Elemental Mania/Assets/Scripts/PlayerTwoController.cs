cf2ce189dd8e9ef0a3eef3baa154da25f718a904
Senad Begovic <senadbegovic2500@hotmail.com>
2018-10-30 10:38:55 +0100

    Added player two alongside the required scripts and modifications to make him be able to move

---
+++ Elemental Mania/Assets/Scripts/PlayerTwoController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class PlayerTwoController : MonoBehaviour {
+    [SerializeField]
+    private float kSpeed;
+    [SerializeField]
+    private float kJumpForce;
+    [SerializeField]
+    private float kMoveInput;
+    [SerializeField]
+    private bool kFacingRight = true;
+
+    private Rigidbody2D kRb;
+    [SerializeField]
+    private Vector2 kMoveVelocity;
+
+    private bool kIsGrounded;
+    [SerializeField]
+    private Transform kGroundCheck;
+    [SerializeField]
+    private float kCheckRadius;
+    [SerializeField]
+    private LayerMask kGroundLayer;
+
+    [SerializeField]
+    private InputMapping kInputMapping;
+
+    void Start ()
+    {
+        kRb = GetComponent<Rigidbody2D>();
+	}
+
+	void Update ()
+    {
+        if (Input.GetButtonDown("PlayerTwo_VerticalMovement") && kIsGrounded)
+        {
+            kRb.velocity = Vector2.up * kJumpForce;
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        kIsGrounded = Physics2D.OverlapCircle(kGroundCheck.position, kCheckRadius, kGroundLayer);
+
+        kMoveInput = Input.GetAxis(kInputMapping.kHorizontalMovement);
+        kRb.velocity = new Vector2(kMoveInput * kSpeed, kRb.velocity.y);
+
+        if(kFacingRight == false && kMoveInput > 0)
+        {
+            flip();
+        } else if(kFacingRight == true && kMoveInput < 0)
+        {
+            flip();
+        }
+   }
+
+    private void flip()
+    {
+        kFacingRight = !kFacingRight;
+        transform.Rotate(Vector2.up, 180);
+    }
+}
