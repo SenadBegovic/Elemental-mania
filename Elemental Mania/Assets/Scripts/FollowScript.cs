c164eb3d709e882b900544a99a3c63ecba625669
Senad Begovic <senadbegovic2500@hotmail.com>
2018-10-30 14:35:34 +0100

    Added size editing on camera, along side with modifying the jump variable and added additionally platforms to the scene

--- Elemental Mania/Assets/Scripts/FollowScript.cs
+++ Elemental Mania/Assets/Scripts/FollowScript.cs
@@ -3,17 +3,39 @@ using System.Collections;
 
 public class FollowScript : MonoBehaviour
 {
-
-    public GameObject player;
-    private Vector3 offset;
+    public Transform player1;
+    public Transform player2;
+    private float minSizeY = 5f;
+    public Camera m_OrthographicCamera;
 
     void Start()
     {
-        offset = transform.position - player.transform.position;
     }
 
     void LateUpdate()
     {
-       transform.position = player.transform.position + offset;
+        SetCameraPos();
+        SetCameraSize();
+    }
+
+    void SetCameraPos()
+    {
+        Vector3 middle = (player1.transform.position + player2.transform.position) * 0.5f;
+        transform.position = new Vector3(
+            middle.x,
+            middle.y,
+            transform.position.z
+        );
+    }
+
+    void SetCameraSize()
+    {
+        m_OrthographicCamera.orthographic = true;
+        float minSizeX = minSizeY * Screen.width / Screen.height;
+        float width = (Mathf.Abs(player1.position.x - player2.position.x) * 0.5f) + 2f;
+        float height = Mathf.Abs(player1.position.y - player2.position.y) * 0.5f;
+        float camSizeX = Mathf.Max(width, minSizeX);
+        m_OrthographicCamera.orthographicSize = Mathf.Max(height,
+            camSizeX * Screen.height / Screen.width, minSizeY);
     }
 }
